301611bb9a54e2107a0c670601b5f1af8d7dfd74
evandro-luiz <evandro07linhares@gmail.com>
2021-01-27 13:18:50 -0300

    27/01-parte 3

--- pcliente/src/environments/environment.prod.ts
+++ pcliente/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyChElQ-EuQx2rf97EUxMwFu7VgPZ-7T2JU",
+    authDomain: "perfeicao-f1b60.firebaseapp.com",
+    databaseURL: "https://perfeicao-f1b60.firebaseio.com",
+    projectId: "perfeicao-f1b60",
+    storageBucket: "perfeicao-f1b60.appspot.com",
+    messagingSenderId: "819800599990",
+    appId: "1:819800599990:web:d02b997e02b4ac3f0ca517"
+  }
 };
